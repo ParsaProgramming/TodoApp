63d219c3365c37273eccec99411263dc7c10ed3b
Parsa <mohammadkazemi.parsa@gmail.com>
2022-09-12 14:46:51 +0430

    add the essential functions

--- javascript/script.js
+++ javascript/script.js
@@ -4,21 +4,40 @@ let ClearBtn = document.querySelector(".buttons button#ClearTodo");
 let list = document.querySelector(".list");
 
 
-function createElem(){
-    let task = input.value;
-    let li = document.createElement("li");
-    li.className = "list-group-item d-flex align-items-center justify-content-between bg-light";
-    let span = document.createElement("span");
-    span.innerHTML = task;
-    let icon = document.createElement("i");
-    icon.className = "fas fa-trash-can text-danger";
-    li.appendChild(span);
-    li.appendChild(icon);
-    list.appendChild(li);
+function ClearInputAndFocus(){
+    input.value = '';
+    input.focus();
 }
 
-function AddTodo(){
+
+function createElem() {
+  let li = document.createElement("li");
+  li.className = "list-group-item d-flex align-items-center justify-content-between bg-light";
+  let span = document.createElement("span");
+  span.innerHTML = input.value;
+  let icon = document.createElement("i");
+  icon.className = "fas fa-trash-can text-danger";
+  li.appendChild(span);
+  li.appendChild(icon);
+  list.appendChild(li);
+
+  icon.addEventListener("click", ()=>{
+    icon.parentElement.remove();
+  })
+
+  ClearInputAndFocus();
+}
+
+function AddTodo() {
+  createElem();
+}
+
+function AddElemWithEnter(e) {
+  if (e.keyCode === 13) {
     createElem();
+  }
 }
 
 AddBtn.addEventListener("click", AddTodo);
+input.addEventListener("keyup", AddElemWithEnter);
+ClearBtn.addEventListener("click", ClearInputAndFocus)
